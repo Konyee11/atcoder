93ee82b5bbc03a22cac6c82a1425be48424b26fb
Konyee11 <konyee.527@gmail.com>
2025-04-11 14:26:06 +0900

    delete space from templates/cpp/main.cpp

--- templates/cpp/main.cpp
+++ templates/cpp/main.cpp
@@ -7,15 +7,13 @@ using namespace std;
 #define debug(x) 0
 #endif
 
-void solve() {
-
-}
+void solve() {}
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
 
-    int t = 1;
-    // cin >> t;
-    while (t--) solve();
+  int t = 1;
+  // cin >> t;
+  while (t--) solve();
 }
